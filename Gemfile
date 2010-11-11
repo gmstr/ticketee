3b548392d708d087f5150693ad3918f858488bad
Gary Strong <gary.m.strong@gmail.com>
2010-11-11 22:25:32 +0000

    Creation of projects feature complete

--- Gemfile
+++ Gemfile
@@ -7,7 +7,8 @@ gem 'rails', '3.0.1'
 
 gem 'sqlite3-ruby', :require => 'sqlite3'
 
-group :test do
+group :development, :test do
+  gem 'rspec'
   gem 'rspec-rails'
 end
 
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ module Ticketee
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => true
+    end
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
